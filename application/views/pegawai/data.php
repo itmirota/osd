1f5b440d32adadb74402b47b02fa44f466e0557a
itmirota <itmirota@gmail.com>
2024-10-24 09:16:11 +0700

    update

--- application/views/pegawai/data.php
+++ application/views/pegawai/data.php
@@ -1,7 +1,7 @@
 <div class="row">
   <div class="d-flex justify-content-end mb-4">
     <button class="btn btn-primary me-2" data-bs-toggle="modal" data-bs-target="#addPegawai"><i class="fa fa-plus"></i> Tambah Data</button>
-    <!-- <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#importCsv"><i class="fa fa-file"></i> Import CSV</button> -->
+    <button class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#importCsv"><i class="fa fa-file"></i> Import CSV</button>
     <div class="form-group">
         <!-- Button trigger modal -->
         <button type="button" class="btn btn-primary me-2" data-bs-toggle="modal" data-bs-target="#exportUser">
