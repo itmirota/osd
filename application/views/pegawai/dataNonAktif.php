9a8592617d2213810a4b3496926ccdab96be2310
itmirota <itmirota@gmail.com>
2025-03-06 09:25:00 +0700

    edit eror tampilan

--- application/views/pegawai/dataNonAktif.php
+++ application/views/pegawai/dataNonAktif.php
@@ -65,11 +65,11 @@
               <a href="" data-bs-toggle="modal" data-bs-target="#detailPegawai" onclick="detailPegawai(<?= $data->id_pegawai?>)"><strong><?= $data->nama_pegawai ?></strong></a>
               <hr class="m-0">
               <span style="font-size:12px"><strong><?= $data->nama_departement ?>/<?= $data->nama_divisi ?></strong></span><br>
-              <span style="font-size:12px"><strong>Area: KANTOR</strong></span><br>
+              <!-- <span style="font-size:12px"><strong>Area: KANTOR</strong></span><br> -->
               <span style="font-size:12px">NIK: MRT<?= $data->nip ?></span>
             </td>
             <td class="text-center"><?= is_null($data->tgl_keluar) ? '-': mediumdate_indo($data->tgl_keluar) ?></td>
-            <td><?= $data->nama_divisi ?></td>
+            <td><?= $data->alasan_keluar ?></td>
             <td><span class="badge <?= ($data->status_pegawai == "tetap" ? 'bg-primary':'bg-info')?>"><?=  ($data->status_pegawai == "tetap" ? 'PKWTT':'PKWT') ?></span></td>
             <?php
             if($role == ROLE_SUPERADMIN | $role == ROLE_HRGA)
@@ -170,7 +170,236 @@
   </div>
 </div>
 
+<!-- Modal Detail Pegawai-->
+<div class="modal fade" id="detailPegawai" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-xl">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5" id="exampleModalLabel">Detail Pegawai</h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <div class="row">
+          <div class="col-6">
+            <div class="mb-3 row">
+              <label for="tempat_tglLahir" class="col-sm-4 col-form-label">Tempat, Tanggal Lahir</label>
+              <div class="col-sm-8">
+                <input type="text" id="tempat_tglLahir" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="shio" class="col-sm-4 col-form-label">Shio</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_shio" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="zodiak" class="col-sm-4 col-form-label">Zodiak</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_zodiak" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_weton" class="col-sm-4 col-form-label">Weton</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_weton" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_jenis_kelamin" class="col-sm-4 col-form-label">Jenis Kelamin</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_jenis_kelamin" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_pendidikan_terakhir" class="col-sm-4 col-form-label">Pendidikan Terakhir</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_pendidikan_terakhir" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_golongan_darah" class="col-sm-4 col-form-label">Golongan Darah</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_golongan_darah" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_agama" class="col-sm-4 col-form-label">Agama</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_agama" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_alamat_ktp" class="col-sm-4 col-form-label">Alamat KTP</label>
+              <div class="col-sm-8">
+                <textarea type="text" id="info_alamat_ktp" readonly class="form-control-plaintext"></textarea>
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_alamat_domisili" class="col-sm-4 col-form-label">Alamat domisili</label>
+              <div class="col-sm-8">
+                <textarea type="text" id="info_alamat_domisili" readonly class="form-control-plaintext"></textarea>
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_kontak" class="col-sm-4 col-form-label">Nomor</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_kontak" readonly class="form-control-plaintext">
+              </div>
+            </div>
+          </div>
+          <div class="col-6">
+            <div class="mb-3 row">
+              <label for="info_ktp" class="col-sm-4 col-form-label">Nomor KTP</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_ktp" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_kk" class="col-sm-4 col-form-label">Nomor KK</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_kk" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_jamsostek" class="col-sm-4 col-form-label">Nomor Jamsostek</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_jamsostek" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_bpjsKesehatan" class="col-sm-4 col-form-label">Nomor Bpjs Kesehatan</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_bpjsKesehatan" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_npwp" class="col-sm-4 col-form-label">Nomor NPWP</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_npwp" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_tgl_masuk" class="col-sm-4 col-form-label">Tanggal Masuk</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_tgl_masuk" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_tgl_masuk" class="col-sm-4 col-form-label">Tanggal Selesai Kontrak</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_tgl_selesai" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_durasi_kontrak" class="col-sm-4 col-form-label">Durasi Kontrak</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_durasi_kontrak" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_email" class="col-sm-4 col-form-label">Email</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_email" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_ibu" class="col-sm-4 col-form-label">Nama Ibu</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_ibu" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_ayah" class="col-sm-4 col-form-label">Nama ayah</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_ayah" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_pasangan" class="col-sm-4 col-form-label">Nama Pasangan</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_pasangan" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_no_pasangan" class="col-sm-4 col-form-label">No. HP Pasangan</label>
+              <div class="col-sm-8">
+                <input type="text" id="info_no_pasangan" readonly class="form-control-plaintext">
+              </div>
+            </div>
+            <div class="mb-3 row">
+              <label for="info_anak" class="col-sm-4 col-form-label">Nama anak</label>
+              <div class="col-sm-8">
+                <textarea id="info_anak" readonly class="form-control-plaintext"></textarea>
+              </div>
+            </div>
+            <hr>
+              <div class="col-md-10">
+                <label for="nama_kontakdarurat1" class="form-label">Kontak Darurat</label>
+              </div>
+              <div class="col-md-10">
+                <label for="nama_kontakdarurat1" class="form-label">Nama Kontak Darurat 1</label>
+                <input type="text" id="info_nama_kontakdarurat1" readonly class="form-control-plaintext" />
+              </div>
+              <div class="row">
+                <div class="col-md-5">
+                  <label for="no_hpdarurat1" class="form-label">No. HP 1</label>
+                  <input type="text" id="info_no_hpdarurat1" readonly class="form-control-plaintext" />
+                </div>
+                <div class="col-md-5">
+                  <label for="hubungan_darurat1" class="form-label">Hubungan Kontak Darurat 1</label>
+                  <input type="text" id="info_hubungan_darurat1" readonly class="form-control-plaintext" />
+                </div>
+              </div>
+              <div class="col-md-10">
+                <label for="nama_kontakdarurat2" class="form-label">Nama Kontak Darurat 2</label>
+                <input type="text" id="info_nama_kontakdarurat2" readonly class="form-control-plaintext" />
+              </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
 <script>
+  function detailPegawai($id){
+    $.ajax({
+      url:"<?php echo site_url("pegawai/detailpegawai")?>/" + $id,
+      dataType:"JSON",
+      type: "get",
+      success:function(hasil){
+
+        const pegawai = hasil.pegawai;
+
+        document.getElementById("tempat_tglLahir").value = pegawai.tempat_lahir+', '+pegawai.tgl_lahir;
+        document.getElementById("info_shio").value = pegawai.shio;
+        document.getElementById("info_zodiak").value = pegawai.zodiak;
+        document.getElementById("info_weton").value = pegawai.weton;
+        document.getElementById("info_jenis_kelamin").value = pegawai.jenis_kelamin == 'L' ? 'Laki-laki':'Perempuan';
+        document.getElementById("info_pendidikan_terakhir").value = pegawai.pendidikan_terakhir+' '+pegawai.jurusan;
+        document.getElementById("info_golongan_darah").value = pegawai.golongan_darah;
+        document.getElementById("info_agama").value = pegawai.agama;
+        document.getElementById("info_alamat_ktp").value = pegawai.alamat_ktp;
+        document.getElementById("info_alamat_domisili").value = pegawai.alamat_domisili;
+        document.getElementById("info_kontak").value = pegawai.kontak_pegawai;
+        document.getElementById("info_kk").value = pegawai.no_kk;
+        document.getElementById("info_ktp").value = pegawai.no_ktp;
+        document.getElementById("info_jamsostek").value = pegawai.no_jamsostek;
+        document.getElementById("info_bpjsKesehatan").value = pegawai.no_bpjsKesehatan;
+        document.getElementById("info_npwp").value = pegawai.no_npwp;
+        document.getElementById("info_tgl_masuk").value = pegawai.tgl_masuk;
+        document.getElementById("info_tgl_selesai").value = pegawai.tgl_selesai;
+        document.getElementById("info_durasi_kontrak").value = pegawai.durasi_kontrak;
+        document.getElementById("info_email").value = pegawai.email_pegawai;
+        document.getElementById("info_ibu").value = pegawai.nama_ibu;
+        document.getElementById("info_ayah").value = pegawai.nama_ayah;
+        document.getElementById("info_pasangan").value = pegawai.nama_pasangan;
+        document.getElementById("info_anak").value = pegawai.nama_anak;
+      }
+    });
+  }
+
   function editData($id){
     $.ajax({
       url:"<?php echo site_url("pegawai/detailpegawai")?>/" + $id,
