0c01dbd6f177a02dc1ac421de0a406ee56d8e62c
itmirota <itmirota@gmail.com>
2025-10-01 08:38:39 +0700

    fix:eror table pelanggaran

--- application/models/Pelanggaran_model.php
+++ application/models/Pelanggaran_model.php
@@ -6,7 +6,7 @@ class Pelanggaran_model extends CI_Model
     $this->db->select('*');
     $this->db->from('tbl_pelanggaran a');
     $this->db->join('tbl_pegawai b','b.id_pegawai = a.pegawai_id');
-    $this->db->join('tbl_bagian c','c.id_bagian = a.bagian_id');
+    $this->db->join('tbl_bagian c','c.id_bagian = b.bagian_id');
     $this->db->join('tbl_divisi d','d.id_divisi = c.divisi_id');
     $this->db->join('tbl_departement e','e.id_departement = d.departement_id');
     $query = $this->db->get();
@@ -18,7 +18,7 @@ class Pelanggaran_model extends CI_Model
     $this->db->select('*');
     $this->db->from('tbl_pelanggaran a');
     $this->db->join('tbl_pegawai b','b.id_pegawai = a.pegawai_id');
-    $this->db->join('tbl_bagian c','c.id_bagian = a.bagian_id');
+    $this->db->join('tbl_bagian c','c.id_bagian = b.bagian_id');
     $this->db->join('tbl_divisi d','d.id_divisi = c.divisi_id');
     $this->db->join('tbl_departement e','e.id_departement = d.departement_id');
     $this->db->where($where);
