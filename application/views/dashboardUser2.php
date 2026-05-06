e886928c3aba2643627bc41e57093ce4f2055260
IT Mirota <itmirota@gmail.com>
2026-05-06 14:49:03 +0700

    fix:hide menu perizinan

--- application/views/dashboardUser2.php
+++ application/views/dashboardUser2.php
@@ -122,8 +122,8 @@
           </div>
         </a>
       </div>
-      <div class="col-3">
-        <a href="<?= base_url('perizinan') ?>">
+      <!-- <div class="col-3">
+        <a href="<?= base_url('perizinan') ?>" class="disabled">
           <div class="d-flex flex-column">
             <div class="d-flex justify-content-center mb-2">
               <img class="img-menu" src="<?= base_url('assets/images/folder.png')?>">
@@ -133,7 +133,7 @@
             </p>
           </div>
         </a>
-      </div>
+      </div> -->
       <div class="col-3">
         <a href="<?= base_url('list-paket'); ?>">
           <div class="d-flex flex-column">
