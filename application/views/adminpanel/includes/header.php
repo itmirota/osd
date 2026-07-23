b83e0a52fbb8a92a14b93eef3ce0133b01445303
IT Mirota <itmirota@gmail.com>
2026-07-23 15:46:34 +0700

    update akses view inventaris

--- application/views/adminpanel/includes/header.php
+++ application/views/adminpanel/includes/header.php
@@ -169,7 +169,6 @@ if($role != ROLE_STAFF){ ?>
         <?php }?>
 
         <!-- INVENTARIS -->
-        <?php if($role == ROLE_SUPERADMIN | $role == ROLE_HRGA){?>
         <li class="sidebar-item has-submenu">
           <a class="sidebar-link" href="#"><i class="fa-solid fa-warehouse"></i> Inventaris<i class="fa fa-angle-down" style="float: right;"></i> </a>
           <ul class="submenu collapse">
@@ -222,7 +221,6 @@ if($role != ROLE_STAFF){ ?>
                     <span>Kerusakan Barang</span>
                   </a>
                 </li>
-                <?php } ?>
               </ul>
             </li>
             <!-- MENU KENDARAAN -->
